265c148eedd6325ccfa1bb218dde5f1c5fbed9a8
AvinashNaruka <135991653+AvinashNaruka@users.noreply.github.com>
2025-12-08 21:50:01 +0530

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -1,25 +1,9 @@
-import pandas_ta as ta
-
-def add_indicators(df):
-    df["ema20"] = ta.ema(df["Close"], 20)
-    df["ema50"] = ta.ema(df["Close"], 50)
-    df["ema200"] = ta.ema(df["Close"], 200)
-    df["rsi"] = ta.rsi(df["Close"], 14)
-
-    macd = ta.macd(df["Close"])
-    df = df.join(macd)
-
-    return df
-
-
 def get_trend(df):
     last = df.iloc[-1]
 
-    # FIXED ambiguous comparison
-    if (last["ema20"] > last["ema50"]) and (last["ema50"] > last["ema200"]):
+    if last["ema20"] > last["ema50"] and last["ema50"] > last["ema200"]:
         return "UP"
-
-    elif (last["ema20"] < last["ema50"]) and (last["ema50"] < last["ema200"]):
+    elif last["ema20"] < last["ema50"] and last["ema50"] < last["ema200"]:
         return "DOWN"
-
-    return "SIDEWAYS"
+    else:
+        return "SIDEWAYS"
