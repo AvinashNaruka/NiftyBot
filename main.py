39285ce499ce4e22ddf34a1bf31dd8e5c7ef8652
AvinashNaruka <135991653+AvinashNaruka@users.noreply.github.com>
2025-12-08 21:42:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from fastapi import FastAPI
-from signals import generate_signal
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-import os
-
-
+from signals import generate_signal
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"msg": "Nifty AI Signal Backend Running"}
+# Serve frontend files
+app.mount("/static", StaticFiles(directory="."), name="static")
 
+# Home → Frontend page
+@app.get("/")
+def frontend():
+    return FileResponse("index.html")
+
+
+# API route
 @app.get("/signal")
 def signal():
     return generate_signal()
