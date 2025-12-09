5f5f8ea96f7005cb3081c8371b3393a28cd8807c
AvinashNaruka <135991653+AvinashNaruka@users.noreply.github.com>
2025-12-09 09:04:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
 from signals import generate_signal
 
 app = FastAPI()
@@ -8,13 +7,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["*"],
     allow_headers=["*"],
+    allow_methods=["*"]
 )
-
-@app.get("/")
-def home():
-    return FileResponse("index.html")
 
 @app.get("/signal")
 def signal(request: Request):
