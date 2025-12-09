f5628db0848c22eb7eb87c935c373de5ecc41918
AvinashNaruka <135991653+AvinashNaruka@users.noreply.github.com>
2025-12-09 09:00:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
-# main.py
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from signals import generate_signal
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 @app.get("/")
-def serve_frontend():
-    # if you have index.html in same folder
-    try:
-        return FileResponse("index.html")
-    except:
-        return {"msg":"Avinash Trading AI backend running"}
+def home():
+    return FileResponse("index.html")
 
 @app.get("/signal")
-def signal():
-    return generate_signal()
+def signal(request: Request):
+    manual_ltp = request.query_params.get("ltp")
+    return generate_signal(manual_ltp)
